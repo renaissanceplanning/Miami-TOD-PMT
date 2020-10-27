488bc16d15044bc36cd150759a7c160f56a18362
Charles Rudder <crudder@citiesthatwork.com>
2020-10-27 16:59:45 -0400

    updates to building permit code

--- Python/config/config_permits.py
+++ Python/config/config_permits.py
@@ -9,11 +9,20 @@ config_path = Path(__file__).parent
 CAT_CODE_PEDOR = "PD"
 CAT_CODE_TBL = Path(config_path, "road_impact_fee_cat_codes.csv")
 
+# status code dict
+STATUS_DICT = dict(
+    [
+        ("C", "Collected"),
+        ("A", "Assessed"),
+        ("L", "Letter of Credit submitted"),
+        ("B", "Bond submitted")
+    ]
+)
 
 FIELDS_DICT = dict(
     [
         ("PROC_NUM", "PROC_NUM"),
-        ("FOLIO_NUM", "PARCEL_NO"),
+        ("FOLIO_NUM", "PARCELNO"),
         ("SITE_ADDR", "ADDRESS"),
         ("ASSE_DATE", "DATE"),
         ("STATUS_CODE", "STATUS"),
@@ -25,7 +34,7 @@ FIELDS_DICT = dict(
 )
 
 USE = FIELDS_DICT.keys()
-DROPS = ["LAT", "LONG", "DATE", "BIKE_TYPE", "PED_TYPE"]
+DROPS = ['CONST_COST', "ADMIN_COST", "CAT_CODE"]
 
 # CRS
 IN_CRS = 4326  # WGS84 latitude/longitude
--- Python/prep_building_permits.py
+++ Python/prep_building_permits.py
@@ -17,9 +17,11 @@ from PMT import (
     RAW,
     CLEANED,
     makePath,
+
 )
 from config.config_permits import (
     FIELDS_DICT,
+    STATUS_DICT,
     USE,
     DROPS,
     IN_CRS,
@@ -129,21 +131,61 @@ if __name__ == "__main__":
         RAW = Path(ROOT, "Raw")
         CLEANED = Path(ROOT, "Cleaned")
     data = Path(RAW, "BuildingPermits", "Road Impact Fee Collection Report -- 2019.csv")
+    parcel_layer = Path(RAW, 'Parcels', 'Miami_2019.shp')
+
     out_path = Path(CLEANED, "BuildingPermits")
     if not out_path.exists():
         Path.mkdir(out_path)
-    out_name = "Miami_Dade_BuildingPermits.csv"
+    out_name = "Miami_Dade_BuildingPermits.shp"
+
+    # read parcels to geodataframe and convert to centroids
+    print('- reading in parcel data to points')
+    parcels = gpd.read_file(parcel_layer)
+    parcel_pts = parcels.copy()
+    parcel_pts['geometry'] = parcel_pts['geometry'].centroid
+
+    # read permit data to dataframe and reformat
+    print('- reading in permit data')
     raw_df = csv_to_df(csv_file=data, use_cols=USE, rename_dict=FIELDS_DICT)
-    raw_df['PARCEL_NO'] = raw_df['PARCEL_NO'].astype(np.str)
-    raw_df['PARCEL_NO'] = raw_df['PARCEL_NO'].apply(lambda x: x.zfill(13))
+    raw_df['COST'] = raw_df['CONST_COST'] + raw_df['ADMIN_COST']    # combine cost
+    print('...combined cost generated')
+
+    # drop fake data - Keith Richardson of RER informed us that any PROC_NUM/ADDRESS that contains with 'SMPL' or
+    # 'SAMPLE' should be ignored as as SAMPLE entry
+    ignore_text = ['SMPL', "SAMPLE", ]
+    for ignore in ignore_text:
+        for col in ['PROC_NUM', 'ADDRESS']:
+            raw_df = raw_df[~raw_df[col].str.contains(ignore)]
+
+    # fix parcelno to string of 13 len
+    raw_df['PARCELNO'] = raw_df['PARCELNO'].astype(np.str)
+    raw_df['PARCELNO'] = raw_df['PARCELNO'].apply(lambda x: x.zfill(13))
+
+    # id project as pedestrain oriented
     raw_df["PED_ORIENTED"] = np.where(raw_df.CAT_CODE.str.contains(CAT_CODE_PEDOR), 1, 0)
+
+    # set landuse codes appropriately accounting for pedoriented dev
     raw_df['CAT_CODE'] = np.where(raw_df.CAT_CODE.str.contains(CAT_CODE_PEDOR),
                                    raw_df.CAT_CODE.str[:-2],
                                    raw_df.CAT_CODE)
+    print('...pedestrian oriented projects identified')
+    # set project status
+    raw_df['STATUS'] = raw_df['STATUS'].map(STATUS_DICT, na_action='NONE')
+    print('...project status set')
+
+    # add landuse codes
     lu_df = pd.read_csv(CAT_CODE_TBL)
-    merge = raw_df.merge(right=lu_df, how="inner", on='CAT_CODE')
-    merge.to_csv(Path(out_path, out_name))
-    print("done")
+    permit_df = raw_df.merge(right=lu_df, how="inner", on='CAT_CODE')
+    print('...land use codes set')
+    permit_df.drop(columns=DROPS, inplace=True)
+
+    # join permits to parcel points MANY-TO-ONE
+    print('...merging permit data to parcel points')
+    permit_points = permit_df.merge(right=parcel_pts, how="inner", on='PARCELNO')
+    permit_points = gpd.GeoDataFrame(permit_points, geometry="geometry")
+    permit_points.to_file(Path(out_path, out_name))
+    print(f"...data written here {out_path}")
+
 
     #TODO: sort out these use codes --> 832-00 (potentially equates to 932-00), 9999-00, 944-00
     #TODO:  they dont appear in the CAT_CODES Table
