bff64aac3be3eb01bda71ce62a91a94436e62a31
Charles Rudder <crudder@citiesthatwork.com>
2020-12-28 16:54:27 -0500

    convert crashes script to arcpy initial commit

--- Python/prep_crashes_nonmotor.py
+++ Python/prep_crashes_nonmotor.py
@@ -14,7 +14,7 @@ Sources inlcude:
 """
 # %% IMPORTS
 import json
-# import geopandas as gpd
+import numpy as np
 import pandas as pd
 import time
 import os
@@ -52,28 +52,28 @@ output_folder = os.path.join(
 )
 
 
-def geojson_to_gdf(geojson, crs, use_cols=USE, rename_dict=FIELDS_DICT):
-    """
-    reads in geojson, drops unnecessary attributes and renames the kept attributes
-    Parameters
-    ----------
-    geojson: json
-        GeoJSON text file consisting of points for bike/ped crashes
-    crs:
-        EPSG code representing
-    use_cols: list
-        list of columns to use in formatting
-    rename_dict: dict
-        dictionary to map existing column names to more readable names
-    Returns
-    -------
-        geodataframe
-    """
-    with open(str(geojson), "r") as src:
-        js = json.load(src)
-        gdf = gpd.GeoDataFrame.from_features(js["features"], crs=crs, columns=use_cols)
-        gdf.rename(columns=rename_dict, inplace=True)
-    return gdf
+# def geojson_to_gdf(geojson, crs, use_cols=USE, rename_dict=FIELDS_DICT):
+#     """
+#     reads in geojson, drops unnecessary attributes and renames the kept attributes
+#     Parameters
+#     ----------
+#     geojson: json
+#         GeoJSON text file consisting of points for bike/ped crashes
+#     crs:
+#         EPSG code representing
+#     use_cols: list
+#         list of columns to use in formatting
+#     rename_dict: dict
+#         dictionary to map existing column names to more readable names
+#     Returns
+#     -------
+#         geodataframe
+#     """
+#     with open(str(geojson), "r") as src:
+#         js = json.load(src)
+#         gdf = gpd.GeoDataFrame.from_features(js["features"], crs=crs, columns=use_cols)
+#         gdf.rename(columns=rename_dict, inplace=True)
+#     return gdf
 
 
 def split_date(gdf, date_field):
@@ -121,8 +121,8 @@ def combine_incidents(gdf, type_dict):
         gdf.loc[mask, "TRANS_TYPE"] = crash_text
 
 
-def clean_crashes_to_GDF(
-        input_path,
+def clean_crashes(
+        input_df,
         out_path,
         usecols=USE,
         rename_dict=INCIDENT_TYPES,
@@ -156,26 +156,57 @@ def clean_crashes_to_GDF(
     """
     if out_path.exists():
         out_path.unlink()
-    bp_gdf = geojson_to_gdf(
-        geojson=input_path, crs=in_crs, use_cols=usecols, rename_dict=FIELDS_DICT
-    )
 
-    split_date(gdf=bp_gdf, date_field="DATE")
-    combine_incidents(gdf=bp_gdf, type_dict=rename_dict)
+    split_date(gdf=input_df, date_field="DATE")
+    combine_incidents(gdf=input_df, type_dict=rename_dict)
     # recode integer coded attributes
-    bp_gdf["CITY"] = bp_gdf["CITY"].apply(lambda x: CITY_CODES.get(int(x), "None"))
-    bp_gdf["HARM_EVNT"] = bp_gdf["HARM_EVNT"].apply(
-        lambda x: HARMFUL_CODES.get(int(x), "None")
-    )
-    bp_gdf["INJSEVER"] = bp_gdf["INJSEVER"].apply(
-        lambda x: SEVERITY_CODES.get(int(x), "None")
-    )
+    input_df["CITY"] = input_df["CITY"].apply(lambda x: CITY_CODES.get(int(x), "None"))
+    input_df["HARM_EVNT"] = input_df["HARM_EVNT"].apply(lambda x: HARMFUL_CODES.get(int(x), "None"))
+    input_df["INJSEVER"] = input_df["INJSEVER"].apply(lambda x: SEVERITY_CODES.get(int(x), "None"))
     # drop unneeded fields
-    bp_gdf.drop(columns=DROPS, inplace=True)
-    # reproject to project CRS
-    bp_gdf.to_crs(epsg=out_crs, inplace=True)
-    # write out to shapefile
-    bp_gdf.to_file(filename=out_path)
+    input_df.drop(columns=DROPS, inplace=True)
+    # write out to featureclass
+    # build array from dataframe
+    temp_fc = r"in_memory\temp_points"
+    shape_fields = ["LONG", "LAT"]
+    from_sr = 4326
+    in_array = np.array(
+        np.rec.fromrecords(
+            input_df.values, names=input_df.dtypes.index.tolist()
+        )
+    )
+    # write to temp feature class
+    arcpy.da.NumPyArrayToFeatureClass(
+        in_array=in_array,
+        out_table=temp_fc,
+        shape_fields=shape_fields,
+        spatial_reference=from_sr,
+    )
+    # reproject if needed, otherwise dump to output location
+    if from_sr != to_sr:
+        arcpy.Project_management(
+            in_dataset=temp_fc, out_dataset=out_fc, out_coor_system=to_sr
+        )
+    else:
+        out_path, out_fc = os.path.split(out_fc)
+        if overwrite:
+            checkOverwriteOutput(output=out_fc, overwrite=overwrite)
+        arcpy.FeatureClassToFeatureClass_conversion(
+            in_features=temp_fc, out_path=out_path, out_name=out_fc
+        )
+    # clean up temp_fc
+    arcpy.Delete_management(in_data=temp_fc)
+
+
+def clean_and_drop(feature_class, use_cols=USE, rename_dict=FIELDS_DICT):
+    # reformat attributes and keep only useful
+    fields = [f.name for f in arcpy.ListFields(feature_class) if not f.required]
+    drop_fields = [f for f in fields if f not in list(use_cols) + ['Shape']]
+    for drop in drop_fields:
+        arcpy.DeleteField_management(in_table=feature_class, drop_field=drop)
+    # rename attributes
+    for name, rename in rename_dict.items():
+        arcpy.AlterField_management(in_table=feature_class, field=name, new_field_name=rename, new_field_alias=rename)
 
 
 def validate_json(json_file):
@@ -190,38 +221,42 @@ def validate_json(json_file):
 
 
 def clean_bike_ped_crashes(
-        input_geojson, out_fc, where_clause=None,
+        in_fc, out_path, out_name, where_clause=None,
         use_cols=USE, rename_dict=FIELDS_DICT):
-    # validate data are json
-    if validate_json(json_file=input_geojson):
-        # define out pathing
-        out_path, out_fc = os.path.split(out_fc)
-        # convert json to temp feature class
-        temp_points = r"in_memory\\crash_points"
-        try:
-            all_features = arcpy.JSONToFeatures_conversion(
-                in_json_file=input_geojson, out_features=temp_points,
-                geometry_type="POINT")
-            # reformat attributes and keep only useful
-            fields = [f.name for f in arcpy.ListFields(all_features) if not f.required]
-            arcpy.AddMessage(fields)
-            drop_fields = [f for f in fields if f not in list(use_cols) + ['Shape']]
-            for drop in drop_fields:
-                arcpy.DeleteField_management(
-                    in_table=all_features, drop_field=drop)
-            # rename attributes
-            for name, rename in rename_dict.items():
-                arcpy.AlterField_management(
-                    in_table=all_features, field=name,
-                    new_field_name=rename, new_field_alias=rename)
-            # utilize where clause to split up points
-            arcpy.FeatureClassToFeatureClass_conversion(
-                in_features=all_features, out_path=out_path,
-                out_name=out_fc, where_clause=where_clause)
-        except Exception as e:
-            arcpy.AddMessage(e)
-            if arcpy.Exists(all_features):
-                arcpy.Delete_management(all_features)
+        # dump subset to new FC
+        out_fc = os.path.join(out_path, out_name)
+        arcpy.FeatureClassToFeatureClass_conversion(
+            in_features=in_fc, out_path=out_path,
+            out_name=out_name, where_clause=where_clause)
+
+        # reformat attributes and keep only useful
+        clean_and_drop(feature_class=out_fc)
+
+        # update city code/injury severity/Harmful event to text value
+        with arcpy.da.UpdateCursor(out_fc, field_names=["CITY", "INJSEVER", "HARM_EVNT"]) as cur:
+            for row in cur:
+                city, severity, event = row
+                if city is not None:
+                    row[0] = CITY_CODES[int(city)]
+                if severity is not None:
+                    row[1] = SEVERITY_CODES[int(severity)]
+                if event is not None:
+                    row[2] = HARMFUL_CODES[int(event)]
+                cur.updateRow(row)
+
+        # combine bike and ped type into single attribute and drop original
+        arcpy.AddField_management(in_table=out_fc, field_name="TRANS_TYPE", field_type="TEXT")
+        fields = ["TRANS_TYPE", "PED_TYPE", "BIKE_TYPE"]
+        with arcpy.da.UpdateCursor(out_fc, field_names=fields) as cur:
+            for row in cur:
+                both, ped, bike = row
+                if ped == "Y":
+                    row[0] = "PEDESTRIAN"
+                if bike == "Y":
+                    row[0] = "BIKE"
+                cur.updateRow(row)
+        for field in fields[1:]:
+            arcpy.DeleteField_management(in_table=out_fc, drop_field=field)
 
 
 if __name__ == "__main__":
@@ -241,15 +276,26 @@ if __name__ == "__main__":
     # cleaned_file = makePath(out_path, out_name)
     # clean_bike_ped_crashes(input_path=data, out_path=cleaned_file, use_cols=USE,
     #                        rename_dict=INCIDENT_TYPES, in_crs=IN_CRS, out_crs=OUT_CRS, )
-    year_gdb_config = setup_project.GDB_CONFIG['geodatabases']['PMT_Year']
     # setup_project.build_year_gdb(TEST)
-    for year in YEARS:
-        if year == 2019:
-            out_gdb = os.path.join(TEST, f"PMT_{year}.gdb")
-            FDS = os.path.join(out_gdb, "Points")
-            out_fc = os.path.join(out_gdb, FDS, 'bike_ped_crashes')
-            year_wc = f'"YEAR" = {year}'
-            clean_bike_ped_crashes(input_geojson=data, out_fc=out_fc, where_clause=year_wc,
-                                   use_cols=USE, rename_dict=FIELDS_DICT)
-
+    # validate data are json
+    if validate_json(json_file=data):
+        try:
+            # convert json to temp feature class
+            temp_points = r"in_memory\\crash_points"
+            all_features = arcpy.JSONToFeatures_conversion(in_json_file=data, out_features=temp_points,
+                                                           geometry_type="POINT")
+            # all_df = pd.DataFrame(arcpy.da.TableToNumPyArray(in_table=all_features,
+            #                                                  field_names=list(FIELDS_DICT.keys())))
+            for year in YEARS:
+                if year == 2019:
+                    out_gdb = os.path.join(TEST, f"PMT_{year}.gdb")
+                    FDS = os.path.join(out_gdb, "Points")
+                    out_name = 'BikePedCrashes'
+                    year_wc = f'"CALENDAR_YEAR" = {year}'
+                    clean_bike_ped_crashes(in_fc=all_features, out_path=FDS, out_name=out_name,
+                                           where_clause=year_wc, use_cols=USE, rename_dict=FIELDS_DICT)
+        except Exception as e:
+            arcpy.AddMessage(e)
+            if arcpy.Exists(all_features):
+                arcpy.Delete_management(all_features)
 
